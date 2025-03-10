bca00f1a70f803ccbeafd2c7ab53cf3434e089d0
Possomato <gabrielb.possomato@gmail.com>
2025-03-10 19:26:25 -0300

    add controller user update

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,4 @@
+const {hash} = require('bcryptjs')
 const AppError = require('../utils/AppError')
 
 const sqliteConnection = require('../database/sqlite')
@@ -12,10 +13,46 @@ class UsersController {
       throw new AppError('Este e-mail já está em uso!')
     }
 
-    await database.run('INSERT INTO users(name, email, password) VALUES (?, ?, ?)', [name, email, password])
+    const hashPassword = await hash(password, 8)
+
+    await database.run('INSERT INTO users(name, email, password) VALUES (?, ?, ?)', [name, email, hashPassword])
 
     return res.status(201).json()
   }
+
+  async update(req, res) {
+    const {name, email} = req.body
+    const {id} = req.params
+
+    const database = await sqliteConnection()
+
+    const user = database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user){
+      throw new AppError('Usuário não encontrado')
+    }
+
+    const userWithUpdatedEmail = database.get('SELECT * FROM users WHERE id = (?)', [email])
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError('Esse e-mail já está em uso')
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return res.json()
+  }
+
 }
 
 module.exports = UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,5 +6,6 @@ const usersController = new UsersController
 const usersRouter = Router()
 
 usersRouter.post('/', usersController.create)
+usersRouter.put('/:id', usersController.update)
 
 module.exports = usersRouter
